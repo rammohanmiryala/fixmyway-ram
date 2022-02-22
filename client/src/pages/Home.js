6e45771b04fcdc31bd6f0d3325ed08e3654aa3c4
ram <rammohan944@gmail.com>
2022-02-22 19:23:41 +1030

    add search

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 
-import React  from "react";
+import React from "react";
 
 import { Icon, Step, Grid } from 'semantic-ui-react';
 
@@ -9,48 +9,49 @@ import PincodeForm from '../components/PincodeForm';
 const Home = () => {
   return (
     <main >
-      <> <div className=" Container flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{
-            border: '0px dotted #1a1a1a'
-          }}><PincodeForm />
+      <> 
+        <div className=" Container flex-row justify-center">
+          <div
+            className="col-12 col-md-10 mb-3 p-3"
+            style={{
+              border: '0px dotted #1a1a1a'
+            }}><PincodeForm />
+          </div>
         </div>
-      </div>
-        <Grid divided='vertically' textAlign="center" >
-          <Grid.Row>
-            <Grid.Column textAlign="center" width={4} style={{ borderRight: '1px dotted #1a1a1a' }}>
-              <Step.Group vertical>
-                <Step completed>
-                  <Icon name='truck' />
-                  <Step.Content>
-                    <Step.Title>Shipping</Step.Title>
-                    <Step.Description>Choose your shipping options</Step.Description>
-                  </Step.Content>
-                </Step>
-                <Step completed>
-                  <Icon name='payment' />
-                  <Step.Content>
-                    <Step.Title>Billing</Step.Title>
-                    <Step.Description>Enter billing information</Step.Description>
-                  </Step.Content>
-                </Step>
-                <Step active>
-                  <Icon name='info' />
-                  <Step.Content>
-                    <Step.Title>Confirm Order</Step.Title>
-                  </Step.Content>
-                </Step>
-              </Step.Group>
-            </Grid.Column>
-            <Grid.Column width={4} textAlign="center" style={{ borderRight: '1px dotted #1a1a1a' }}  >
-            <h1>rammohan</h1>
-            </Grid.Column>
-            <Grid.Column width={4} textAlign="center">
-              <h1>rammohan</h1>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+          <Grid divided='vertically' textAlign="center" >
+            <Grid.Row>
+              <Grid.Column textAlign="center" width={4} style={{ borderRight: '1px dotted #1a1a1a' }}>
+                <Step.Group vertical>
+                  <Step completed>
+                    <Icon name='truck' />
+                    <Step.Content>
+                      <Step.Title>Shipping</Step.Title>
+                      <Step.Description>Choose your shipping options</Step.Description>
+                    </Step.Content>
+                  </Step>
+                  <Step completed>
+                    <Icon name='payment' />
+                    <Step.Content>
+                      <Step.Title>Billing</Step.Title>
+                      <Step.Description>Enter billing information</Step.Description>
+                    </Step.Content>
+                  </Step>
+                  <Step active>
+                    <Icon name='info' />
+                    <Step.Content>
+                      <Step.Title>Confirm Order</Step.Title>
+                    </Step.Content>
+                  </Step>
+                </Step.Group>
+              </Grid.Column>
+              <Grid.Column width={4} textAlign="center" style={{ borderRight: '1px dotted #1a1a1a' }}  >
+                <h1>rammohan</h1>
+              </Grid.Column>
+              <Grid.Column width={4} textAlign="center">
+                <h1>rammohan</h1>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
       </>
     </main>
   );
